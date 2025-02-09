5e5483c786bc7ac692a2c077f07403d56a47e29a
Abdaurhman Mabrouk <aabdo6629@gmail.com>
2025-02-09 21:12:36 +0200

    -

--- src/Components/Sections/GameTopNav/GameTopNav.js
+++ src/Components/Sections/GameTopNav/GameTopNav.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import './GameTopNav.css';
 import LoadingModal from '../../Indivaduals/LoadingModal/LoadingModal';
-// import handleNewGame from '../GameTable/GameTable.js';
+import handleNewGame from '../GameTable/GameTable.js';
+import handleStartGame from '../GameTable/GameTable.js';
 
 function GameTopNav() {
+  const handleStartGame = () => {};
   const handleNewGame = () => {
     window.location.reload();
   };
-
   const handlePauseGame = () => {};
   const handleResumeGame = () => {};
   const handleExitGame = () => {};
 
   return (
     <nav id="topNav">
+      <a href="#" /*onClick={}*/ className="top-nav-link link">
+        Start
+      </a>
+
       <a href="#" onClick={handleNewGame} className="top-nav-link link">
         New Game
       </a>
+
       <a href="." /*onClick={``}*/ className="top-nav-link link">
         Pause
       </a>
+
       <a href="." /*onClick={``}*/ className="top-nav-link link">
         Resume
       </a>
+
       <a href="#" /*onClick={``}*/ className="top-nav-link link">
         Exit
       </a>
